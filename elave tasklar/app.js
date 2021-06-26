6a60a78a6c79f594ec3a0eeee40e1fb02c1ef3b5
Camil Ezizov <camilezizli@gmail.com>
2021-06-26 10:51:31 +0400

    newtask

---
+++ elave tasklar/app.js
@@ -0,0 +1,13 @@
+let count=0;
+let res=" ";
+function write(a){
+    for (let i = 1; i < 200; i++) {
+        if(i%2==1){
+            count++;
+           res+=count+" ci  tək nomrə:"+i+"  ";
+        }
+    }  
+    return res;
+}
+write=write(200);
+console.log(write)
